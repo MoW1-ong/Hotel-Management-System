d82e1947151f3c634b8a12f6316756053faaafbc
MoW1-ong <ongsh-wm24@student.tarc.edu.my>
2025-09-12 15:54:22 +0800

    e

---
+++ MainPage.java
@@ -0,0 +1,14 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MainPage{
+    public static void main(String[] args){
+        new Security();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            System.out.println("\n");
+            
+        }
+    }
+}
--- security.java
+++ security.java
@@ -1,4 +1,4 @@
-
+package assignment;
 import java.util.ArrayList;
 import java.util.Scanner;
 
